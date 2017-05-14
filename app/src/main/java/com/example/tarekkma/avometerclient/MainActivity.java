5a0cb9fba9b627e225172064eb2aa1ab74f9d283
Tarekk Mohamed Abdalla <tarekkma@gmail.com>
2017-05-14 10:13:27 +0200

    ready for prod

--- app/src/main/java/com/example/tarekkma/avometerclient/MainActivity.java
+++ app/src/main/java/com/example/tarekkma/avometerclient/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.tarekkma.avometerclient;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
 
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.example.tarekkma.avometerclient.fragments.AboutFragment;
 import com.example.tarekkma.avometerclient.fragments.BookFragment;
 import com.example.tarekkma.avometerclient.fragments.DisplayFragment;
@@ -44,6 +48,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        final String firstTime = "LPKO";
+
+        if(prefs.getBoolean(firstTime,true)){
+            new MaterialDialog.Builder(this)
+                    .content("للإستفادة من التطبيق يجب عليك الحصول علي الجهاز الخاص بالتطبيق" +
+                            "\n" +
+                            "\n" +
+                            "للحصول علي الجهاز :\n" +
+                            "01008659599 م/ حسام حسن" +
+                            "\n" +
+                            "01013658016 م/ طارق محمد")
+                    .positiveText("حسناً")
+                    .neutralText("عدم الإظهار مجدداً")
+                    .onNeutral(new MaterialDialog.SingleButtonCallback() {
+                        @Override
+                        public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                            prefs.edit().putBoolean(firstTime,false).apply();
+                        }
+                    })
+            .show();
+        }
 
         container = (FrameLayout) findViewById(R.id.content);
         getSupportFragmentManager().beginTransaction().replace(R.id.content, DisplayFragment.newInstance()).commit();
